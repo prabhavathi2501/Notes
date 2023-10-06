f82963d7533c7a763583aaf6c2767b3f17bb16c1
prabhavathi2501 <prabhashok2501@gmail.com>
2023-10-06 15:50:38 +0530

    Notes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "./components/Sidebar"
+import Notes from "./components/Notes"
+
+function App() {
+ 
+  return <>
+     <div className='wapper'>
+    <Sidebar/>
+  
+    </div>
+   
+    </>
+  
+}
+
+export default App
